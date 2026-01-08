5686c249ec5c303b2f5b5de819cb04abd3f31306
Sean Chen <schen200826@gmail.com>
2026-01-08 14:48:27 -0500

    place ship

--- game.c
+++ game.c
@@ -3,7 +3,10 @@
 void clear_board(Board *b){
   for (int i = 0; i < sizeof(B->grid); i++) {
     for (int j = 0; j < sizeof(B->grid[0]); j++) {
-        array[i][j] = 0;
+        B->grid[i][j] = 0;
     }
 }
+}
+int place_ship(Board *b, int row, int col){
+  B->grid[row][col] = 1; // one is ship 
 }
