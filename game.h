bd429b5f7cbea0f106ca0d454bd2a7ec0a6b7789
Sean Chen <schen200826@gmail.com>
2026-01-07 20:33:42 -0500

    game.h

---
+++ game.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+struct board{
+    int ships_remaining;
+    char grid[3][3];
+}
+void clear_board(Board *b);
+int place_ship(Board *b, int row, int col); // row a b c col 1 2 3 
+int fire(Board*b, int row, int col);
+int bomb(Board *b, int row, int col);
+int game_over(Board *b);
+void print_board(Board *b);                                                                                     
+
+#endif
