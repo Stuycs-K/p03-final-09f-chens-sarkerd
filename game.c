6cb7be7fa2dcd73cef3f4d192c56640ec8f25a8e
Sean Chen <schen200826@gmail.com>
2026-01-08 14:42:57 -0500

    clear board

--- game.c
+++ game.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-#include "game.h"   
+#include "game.h"
+void clear_board(Board *b){
+  for (int i = 0; i < sizeof(B->grid); i++) {
+    for (int j = 0; j < sizeof(B->grid[0]); j++) {
+        array[i][j] = 0;
+    }
+}
+}
