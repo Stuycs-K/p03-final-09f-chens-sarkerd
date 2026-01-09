5ecaa1c9057b61b5cd8957c6bfea7f3ecda8b2e7
Sean Chen <schen200826@gmail.com>
2026-01-09 17:43:43 -0500

    compiling game.c, going to work on implementing game into the server client connections

--- game.c
+++ game.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "game.h"
-void clear_board(Board *b){
-  for (int i = 0; i < sizeof(B->grid); i++) {
-    for (int j = 0; j < sizeof(B->grid[0]); j++) {
+void clear_board(struct Board *b){
+  b->ships_remaining = 0;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
         b->grid[i][j] = '.';
     }
 }
 }
-int place_ship(Board *b, int row, int col){
+int place_ship(struct Board *b, int row, int col){
   if(b->grid[row][col] == '.'){
     b->grid[row][col] = 'S';
     b->ships_remaining++;
@@ -16,7 +17,27 @@ int place_ship(Board *b, int row, int col){
   return 0;
 }
 
-int print_board(struct Board *b){
+int fire(struct Board *b, int row, int col){
+  if(b->grid[row][col] == 'S'){
+    b->grid[row][col] = 'X';
+     b->ships_remaining--;
+     return 1;
+  }
+  if(b->grid[row][col] == '.'){
+    b->grid[row][col]= 'O';
+  }
+  return 0;
+}
+
+int game_over(struct Board *b){
+  if(b->ships_remaining == 0){
+    return 1;
+  }
+  return 0;
+}
+
+
+void print_board(struct Board *b){
   printf(" A B C\n");
   for(int i = 0;i <3;i++){
     printf("%d ",i+1);
